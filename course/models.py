4ebb9c57afa84d39d7ac2d01871c1e693ad04ae8
Godswill <orjigodswill47@gmail.com>
2024-01-01 15:31:33 +0100

    Create image field for the course db

--- course/models.py
+++ course/models.py
@@ -1,12 +1,66 @@
+import os
 from django.db import models
+from django.db.models.query import QuerySet
 from django.urls import reverse
 from django.conf import settings
+import random
+from django.db.models import Q
+
+
+class CourseQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(status='published')
+
+    def search(self, query):
+        lookups = (
+            Q(title__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        return self.filter(lookups).distinct()
+
+
+class CourseManager(models.Manager):
+    def get_queryset(self):
+        return CourseQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910233549)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(
+        new_filename=new_filename, ext=ext)
+
+    return "course/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
 
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True, null=True, unique=True)
     description = models.TextField()
+    image = models.ImageField(
+        upload_to=upload_image_path, null=True, blank=True)
+
+    objects = CourseManager()
+    search = CourseManager()
 
     def __str__(self):
         return self.title
